33cc43ff20fb874ff11f7d278c7a158fcc8f78d0
1129NF <4keh1to@gmail.com>
2025-07-22 12:33:28 +0900

    検索機能の追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,12 @@
 class Book < ApplicationRecord
+  def self.search(keyword, category)
+    books = Book.all
+    if keyword.present?
+      books = books.where("title LIKE ? OR author LIKE ?", "%#{keyword}%", "%#{keyword}%")
+    end
+    if category.present?
+      books = books.where(category: category)
+    end
+    books
+  end
 end
